76ea1ec88668af7bb67c44a2e8f2de8beae0710e
Mai Leyda <mai.toyonishi@yahoo.com>
2024-01-02 18:16:22 -0600

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,24 @@ import './App.css';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("Component now rendering...")
+  console.log(featPupId);
 
   console.log("puppyList: ", puppyList);
   
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
-      }
-    </div>
-  );
+      {featPupId && <p>Featured Pup Id: {featPupId}</p>}
+      {puppies.map((puppy) => {
+        return (
+        <p onClick={handleClick} key={puppy.id}>
+          {puppy.name}
+        </p>
+      );
+    })}+
+  </div>
+);
 }
 
 export default App
