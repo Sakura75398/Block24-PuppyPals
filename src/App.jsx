325fefdf911eb85c4b3ee289e33f3f47d448e40d
Mai Leyda <mai.toyonishi@yahoo.com>
2024-01-02 18:18:19 -0600

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
 import { useState } from "react";
-import { puppyList } from './data.js';
-import './App.css';
+import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-  console.log("Component now rendering...")
+  console.log("Component now rendering...");
   console.log(featPupId);
 
   console.log("puppyList: ", puppyList);
-  
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  {
+    featPupId && (
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      {featPupId && <p>Featured Pup Id: {featPupId}</p>}
       {puppies.map((puppy) => {
         return (
-        <p onClick={handleClick} key={puppy.id}>
-          {puppy.name}
-        </p>
-      );
-    })}+
-  </div>
-);
+          <p
+            onClick={() => {
+              console.log("puppy id: ", puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
